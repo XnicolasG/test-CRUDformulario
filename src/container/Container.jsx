024987d7312e47bfb4b779cb32ede7b60f8825fe
Nicolás García <nicolasg.ngl@gmail.com>
2022-02-26 22:04:54 -0500

    link repositorio

--- src/container/Container.jsx
+++ src/container/Container.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Formulario from '../components/Formulario'
 import '../styles/MainStyles.css'
+import { GoMarkGithub } from 'react-icons/go'
 
 const Container = () => {
   return (
     <div className='ContPadre'>
-        <Formulario />
-
+      <div className="btn-wrap">
+        <span>Repositorio</span>
+        <div className="container">
+          <center>
+          <a target='_blank' href="https://github.com/XnicolasG/test-CRUDformulario" className="fab">
+            <GoMarkGithub />
+          </a>
+          </center>
+          </div>
+      </div>
+      <Formulario />
     </div>
   )
 }
